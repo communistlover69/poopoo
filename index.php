37c60cc2528d9a61e50bad669c0d524f9a92f1c1
communistlover69 <i2009311@mvrht.net>
2017-07-12 21:25:40 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+<!--
+ * Created by PhpStorm.
+ * User: AntiCom Main Programmer
+ * Date: 5/13/2017
+ * Time: 7:06 PM
+ */
+ -->
+
+<html>
+	<head lang="en">
+		<?php
+		//insert meta tags and links to necessary bootstrap/scss/jquery files
+		require_once 'util/header.php';
+		?>
+		<title>Anticom</title>
+	</head>
+	<body>
+			<section class="navcontainer">
+				<nav role="navigation">
+					<div class="logo logo-font">ANTICOM</div>
+					<ul>
+						<li class="nav-font"> Item 3</li>
+						<li class="nav-font"> Item 2</li>
+						<li class="nav-font"> Item 1</li>
+					</ul>
+				</nav>
+			</section>
+		<section class="hero">
+			<div class = "inner-hero hero-font">
+				All Classes Against Marxism
+			</div>
+		</section>
+		<section class="mission-container">
+			<div class="upper-mission upper-mission-font"> A Catch Phrase</div>
+			<div class="lower-mission lower-mission-font">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque augue, ultricies sed tincidunt quis, euismod vitae est. Morbi tincidunt egestas tellus, quis commodo lectus mollis eu. Cras nec nibh arcu. Vestibulum elit arcu, suscipit vel condimentum at, dignissim id lectus. Aliquam metus odio, laoreet quis odio ut, porta aliquam sem. Nullam ullamcorper vulputa. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque augue, ultricies sed tincidunt quis, euismod vitae est. Morbi tincidunt egestas tellus, quis commodo lectus mollis eu. Cras nec nibh arcu. Vestibulum elit arcu, suscipit vel condimentum at, dignissim id lectus. Aliquam metus odio, laoreet quis odio ut, porta aliquam sem. Nullam ullamcorper vulputa
+			</div>
+		</section>
+		<section class="goal-container">
+			<section class="left-container goal-content-font">
+				<div>
+					<img class="goal-image"  src="util/css/bureau.png" alt="Bureau" />
+				</div>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque augue, ultricies sed tincidunt quis, euismod vitae est. Morbi tincidunt egestas tellus, quis commodo lectus mollis eu. Cras nec nibh arcu. Vestibulum elit arcu, suscipit vel condimentum at, dignissim id lectus. Aliquam metus odio, laoreet quis odio ut, porta aliquam sem. Nullam ullamcorper vulputa
+			</section>
+			<section class="middle-container goal-content-font">
+				<div><img src="util/css/bureau.png" class="goal-image" alt="Bureau"></div>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque augue, ultricies sed tincidunt quis, euismod vitae est. Morbi tincidunt egestas tellus, quis commodo lectus mollis eu. Cras nec nibh arcu. Vestibulum elit arcu, suscipit vel condimentum at, dignissim id lectus. Aliquam metus odio, laoreet quis odio ut, porta aliquam sem. Nullam ullamcorper vulputa
+			</section>
+			<section class="right-container goal-content-font">
+				<div>
+					<img class="goal-image" src="util/css/bureau.png" alt="Bureau"/>
+				</div>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque augue, ultricies sed tincidunt quis, euismod vitae est. Morbi tincidunt egestas tellus, quis commodo lectus mollis eu. Cras nec nibh arcu. Vestibulum elit arcu, suscipit vel condimentum at, dignissim id lectus. Aliquam metus odio, laoreet quis odio ut, porta aliquam sem. Nullam ullamcorper vulputa
+			</section>
+		</section>
+	</body>
+
+</html>
